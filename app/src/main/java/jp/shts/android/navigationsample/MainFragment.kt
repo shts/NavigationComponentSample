5a29a385681ac3c0634c07672b0ea3b67137a927
shts <shts.dev@gmail.com>
2018-09-27 13:38:26 +0900

    Initial commit

---
+++ app/src/main/java/jp/shts/android/navigationsample/MainFragment.kt
@@ -0,0 +1,24 @@
+package jp.shts.android.navigationsample
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        nextButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mainFragment_to_nextFragment)
+        }
+    }
+}
