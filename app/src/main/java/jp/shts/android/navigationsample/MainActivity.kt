b513745db6ce7ee9b06599d1bf143a9758ed445d
shts <shts.dev@gmail.com>
2018-09-28 09:52:08 +0900

    Add tab

--- app/src/main/java/jp/shts/android/navigationsample/MainActivity.kt
+++ app/src/main/java/jp/shts/android/navigationsample/MainActivity.kt
@@ -2,11 +2,17 @@ package jp.shts.android.navigationsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.my_nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNav, navController)
     }
 }
